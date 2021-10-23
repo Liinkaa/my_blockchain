d65ef96440c3a5e86c47127760301414b747900a
Liinkaa <58215967+Liinkaa@users.noreply.github.com>
2021-10-23 12:00:49 +0200

    Add files via upload

---
+++ blockchain/src/file_storage/file_storage_config.py
@@ -0,0 +1,13 @@
+"""Module with configuration and constants"""
+
+JSON_FILES_PATH = "../storage/"
+WALLET_FILES_PATH = "../storage/wallet_booklets/"
+REGISTRATION_FILE = "accountsStore.json"
+BLOCKCHAIN_LOG_FILE = "blockchainLogStore.json"
+JSON_EXTENSION_FILE = ".json"
+
+ENCRYPTION_FOLDER_PATH = "../storage/encryption/"
+DIRECTORY_EXTENSION = "/"
+KEY_FILE_NAME = "key"
+EXTRA_FILE_NAME = "extra"
+NONCE_FILE_NAME = "nonce"
